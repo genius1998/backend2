f1bbcffc251ffdf1cfeb2dacbc83e03a6694a19c
genius1998 <effort98@naver.com>
2024-11-26 20:41:03 +0900

    # hateos 적용전

--- src/main/java/com/example/demo2/config/SecurityConfig.java
+++ src/main/java/com/example/demo2/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 엔드포인트에 대해
-                        .allowedOrigins("http://localhost:3000") // 프론트엔드 도메인 허용
+                        .allowedOrigins("http://localhost:3000", "http://172.30.126.174:8080", "http://172.30.126.174:3000") // 프론트엔드 도메인 허용
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메소드
                         .allowedHeaders("*") // 모든 헤더 허용
                         .allowCredentials(true); // 자격 증명 허용
